68e46310ab36629012ce8a1fb16f892b9a40a52c
Zan <zan.montieth@gmail.com>
2023-04-05 07:44:34 -0600

    introduce a dynamic function to keep the square shape

--- draw.js
+++ draw.js
@@ -2,7 +2,24 @@ function generateHexGrid(gridXdimension, gridYdimension) {
     const parentContainer = document.getElementById("drawingArea");
     let hexElement = document.createElement("div");
     hexElement.classList.add("hex");
-    for (numberOfHexes = 0; numberOfHexes < (gridYdimension * gridXdimension - (gridYdimension/2)); numberOfHexes++) {
+    for (
+        numberOfHexes = 0;
+        numberOfHexes < gridYdimension * gridXdimension - gridYdimension / 2;
+        numberOfHexes++
+    ) {
         parentContainer.appendChild(hexElement.cloneNode(true));
     }
+    makeTheGridASquare(gridXdimension);
+}
+
+function makeTheGridASquare(hexRowLength) {
+    const sizeContainer = document.getElementById("sizeConstraint");
+    const hexElement = document.getElementsByClassName("hex")[0];
+    let hexWidth = getComputedStyle(hexElement).getPropertyValue("width");
+    hexWidth = hexWidth.substring(0, hexWidth.length - 2);
+    let hexMargin = getComputedStyle(hexElement).getPropertyValue("margin-left");
+    hexMargin = hexMargin.substring(0, hexMargin.length - 2);
+    let newWidth = hexWidth * hexRowLength + hexMargin * 2 * hexRowLength;
+    newWidth = newWidth + "px"
+    sizeContainer.style.width = newWidth
 }
